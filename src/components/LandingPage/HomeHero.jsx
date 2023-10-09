257e91dd09260e7aaee28c677d43301b192020ca
adindaaasals <adindaaasals28@gmail.com>
2023-10-10 05:42:36 +0700

    update style tagline

--- src/components/LandingPage/HomeHero.jsx
+++ src/components/LandingPage/HomeHero.jsx
@@ -9,10 +9,10 @@ function HomeHero() {
         id="home"
       >
         <div className="">
-          <h1 className="text-4xl font-medium text-[#22467C] text-center">
+          <h1 className="text-5xl font-bold text-[#4F4A43] text-center">
             Satukan tekad, satukan tujuan!
           </h1>
-          <h1 className="text-5xl font-bold text-[#4F4A43] text-center">
+          <h1 className="text-4xl font-medium text-[#22467C] text-center">
             Marilah kita sama-sama kurangi entisitas dari Demam Berdarah Dengue (DBD)
           </h1>
         </div>
