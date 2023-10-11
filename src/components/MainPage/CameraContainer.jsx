b90f8236186434cb536fd98d46e73c10b272c19f
adindaaasals <adindaaasals28@gmail.com>
2023-10-11 14:32:04 +0700

    update fesp 32 cam frame

--- src/components/MainPage/CameraContainer.jsx
+++ src/components/MainPage/CameraContainer.jsx
@@ -4,8 +4,8 @@ function CameraContainer() {
   return (
     <>
       <div className='flex justify-center'>
-        <div className='w-[586px] h-[400px] bg-slate-800 rounded-3xl m-16'>
-          <iframe className='w-[586px] h-[400px]' src="http://172.20.10.2"></iframe>
+        <div className='w-[350px] h-[270px] bg-slate-800 rounded-3xl m-16'>
+          <iframe className='w-[350px] h-[270px]' src="http://172.20.10.2"></iframe>
         </div>
       </div>
     </>
