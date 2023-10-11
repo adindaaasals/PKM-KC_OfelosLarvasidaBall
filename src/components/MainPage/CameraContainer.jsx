2e4cac52b1c9f6a2f7a875472213afcba59b3752
adindaaasals <adindaaasals28@gmail.com>
2023-10-11 14:14:19 +0700

    update frame esp32 cam

--- src/components/MainPage/CameraContainer.jsx
+++ src/components/MainPage/CameraContainer.jsx
@@ -5,7 +5,7 @@ function CameraContainer() {
     <>
       <div className='flex justify-center'>
         <div className='w-[586px] h-[400px] bg-slate-800 rounded-3xl m-16'>
-          <iframe src="http://172.20.10.2"></iframe>
+          <iframe className='w-[586px] h-[400px]' src="http://172.20.10.2"></iframe>
         </div>
       </div>
     </>
